430d938cb9889801aa973c00da520d6e963587bd
Massimo, Caputo <massimo.caputo@accenture.com>
2024-05-03 19:36:40 +0200

    Initial commit

---
+++ src/main/java/com/acn/nemo/MvcCrudExampleApplication.java
@@ -0,0 +1,13 @@
+package com.acn.nemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcCrudExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcCrudExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/acn/nemo/MvcCrudExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acn.nemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCrudExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
